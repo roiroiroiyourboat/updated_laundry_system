4157e8dd9392132137b6bcb82a977bf8f7490917
roiroiroiyourboat <94413481+roiroiroiyourboat@users.noreply.github.com>
2024-11-03 00:17:35 +0800

    logout

--- laundry_system/main/homepage/logout.php
+++ laundry_system/main/homepage/logout.php
@@ -5,4 +5,11 @@ if (session_destroy()) {
     header("Location: /laundry_system/main/homepage/homepage.php");
     exit();
 }
+
+$conn = new mysqli('localhost', 'root', '', 'laundry_db');
+
+if ($conn->connect_error) {
+    die(json_encode(['status' => 'error', 'message' => "Connection error: " . $conn->connect_error]));
+}
+
 ?>
